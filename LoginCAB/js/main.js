7a20dd886a18620549fa48fdb9996e84373ba8b7
henriqueMors <ahmdesouza@gmail.com>
2024-05-16 17:09:37 -0300

    update

--- LoginCAB/js/main.js
+++ LoginCAB/js/main.js
@@ -11,3 +11,28 @@ $(function() {
 	});
 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById('login-form');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const errorMessage = document.getElementById('error-message');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = usernameInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        if (username === 'admin@emailadmin.com' && password === 'adminadmin10') {
+            // Redireciona para a página autenticada
+            window.location.href = 'connectautobio.html'; // Substitua 'pagina_autenticada.html' pelo URL da sua página autenticada
+        } else {
+            // Exibe a mensagem de erro
+            errorMessage.textContent = 'Usuário ou senha incorretos';
+            // Limpa os campos de entrada
+            usernameInput.value = '';
+            passwordInput.value = '';
+        }
+    });
+});
